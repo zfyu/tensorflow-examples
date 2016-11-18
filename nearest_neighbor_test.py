e58dc6bf4296d7d506a58f7f50b19abcf65fddcd
yuzongfu <zongfuyu@hotmail.com>
2016-11-18 17:00:17 +0800

    add nearest_neighbor and some practice

---
+++ nearest_neighbor_test.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+
+Xtr,Ytr = mnist.train.next_batch(50)
+Xte,Yte = mnist.test.next_batch(4)
+
+xtr = tf.placeholder("float",[None,784])
+xte = tf.placeholder("float",[784])
+
+distance = tf.reduce_sum(tf.abs(tf.add(xtr,tf.neg(xte))),reduction_indices=1)
+
+pred = tf.arg_min(distance,0)
+accuracy = 0
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+	sess.run(init)
+	print(len(Xte))
+	for i in range(len(Xte)):
+		nn_index = sess.run(pred,feed_dict={xtr:Xtr,xte:Xte[i,:]})
+		print(sess.run(distance,feed_dict={xtr:Xtr,xte:Xte[i]}))
+		print(nn_index)
+		#print("Test",i,"Prediction:",np.argmax(Ytr[nn_index]),"True Class:",np.argmax(Yte[i]))
+		if (np.argmax(Ytr[nn_index]) == np.argmax(Yte[i])):
+			accuracy +=1./len(Xte)
+	print("Done!")
+	print("Accuracy:",accuracy)
